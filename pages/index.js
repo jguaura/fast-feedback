4345b38de6ba45897effd014b4f9f0165e22a15d
Jose R. Guaura <jguaura@gmail.com>
2022-05-16 18:33:23 -0300

    update-headline: changed title

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          WWWelcome <a href="https://nextjs.org">Next.js!</a>
+          Hellow World
         </h1>
 
         <p className={styles.description}>
