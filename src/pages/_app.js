7a167ec5d29f3a6361eb0e50511cc4538589ffe7
Jose Rafael Guaura <jguaura@gmail.com>
2022-05-18 01:18:50 -0300

    style(chakra-ui): theme provider + custom theme
    Added chakra provider to _app.js - Extended custom theme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,15 @@
 import { AuthProvider } from '../lib/auth';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from '../styles/theme';
 
 const App = ({ Component, pageProps }) => {
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </AuthProvider>
   );
-}
+};
 
 export default App;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,21 @@
 import Head from 'next/head';
-import Image from 'next/image';
 import { useAuth } from '../lib/auth';
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
   const auth = useAuth();
 
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Fast Feedback</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Fast feedback</h1>
-        <p className={styles.description}>
+      <main>
+        <h1>Fast feedback</h1>
+        <p>
           Current user{' '}
-          <code className={styles.code}>
+          <code >
             {auth?.user?.displayName}
             {auth?.user?.email}
           </code>
@@ -30,19 +28,6 @@ export default function Home() {
           )}
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 }
